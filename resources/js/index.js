fead98db0902a9e3332575bc2d092a1e3320a9e6
Your Name <janik.schoebel@gmail.com>
2021-06-10 18:51:14 +0200

    WeatherWidget ausgelagert, ESLint konform

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,26 +1,16 @@
 /* eslint-env browser */
 
-/**
- * TODO:
- * 
- * - Eingaben der Nutzer*innen im Inputfeld abfangen
- * - Prüfen ob eingegebene Städte mit der OpenWeatherMap-API verwendet werden können
- * - Neues WetterWidget in Liste ergänzen, Wetterdaten für die eingegebene Stadt holen und im Widget anzeigen
- * - Hinzugefügte Stadt im LocalStorage "merken" und beim erneuten Starten der App für alle gespeicherten Städte Widgets zur liste hinzufügen
- * - Schaltflächen des Widget-Template nutzen um angezeigte Städte aktualisieren und entfernen zu können
- * - Optional: Liste per Drag & Drop sortierbar machen
- * - ...
- */
  import DataProvider from "./utils/DataProvider.js";
  import InputBar from "./ui/InputBar.js";
+ import WeatherWidget from "./ui/WeatherWidget.js";
  
  const NOT_FOUND = "404";
  var inputBar = new InputBar,
+     weatherWidget = new WeatherWidget,
      cityList = new Array();
 
     //Die im localstorage gespeicherten Städte werden geladen und die InputBar initialisiert.
  function init() {
-     console.log("### Starting Weather-App ###");
      addFromLocalStorage();
      initInput();
  }
@@ -37,8 +27,8 @@
              });
             }
           else {
-             addWeatherWidget(data);
-             
+            weatherWidget.addWeatherWidget(data,litter,getData,deleteFromLocalStorage);
+            saveInLocaleStorage(city);
          }
      });
  }
@@ -49,121 +39,9 @@
          if(e.key === "Enter" && inputBar.bar.value !== ""){
              let input = inputBar.getInput();
              getData(input);
-             saveInLocaleStorage(input);
+             
          }
      });
- }
-
- function addWeatherWidget(data){
-
-    //Prüfen ob Stadt bereits in Liste existiert, falls nicht hinzufügen, falls doch aktualisieren
-    var WidgetList = document.getElementsByTagName('ul')[0];
-    var CityExists = 0
-    if (WidgetList.childElementCount > 1) // Nur Ausführen wenn Bereits eine Stadt in der Liste enthalten ist
-    {
-        for(var i = 1; i < WidgetList.children.length; i++)
-        {
-            if(data.name === WidgetList.children[i].attributes[1].value){
-                CityExists = i;
-                break;
-            }
-        }        
-    }
-
-    if(CityExists > 0) //Stadt Existiert bereits
-    {
-        //Icon anpassen
-        //"http://openweathermap.org/img/wn/10d@2x.png"
-        var iconsource = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        var icon = WidgetList.children[i].querySelector('.icon');
-        icon.children[0].src = iconsource;    
-        
-        //Ort + Temperatur Befüllen
-        // Template --> span.data --> span.main
-
-        var CityandTemp = WidgetList.children[i].querySelector('.main');
-        var CurrentString = CityandTemp.querySelector('span')
-        CurrentString.textContent = data.name + ", " + Math.round(data.main.temp) + "°C";
-    
-        //min Temperature
-        var MinTemperature = WidgetList.children[i].querySelector('.container.min-temperature');
-        MinTemperature.innerText = Math.round(data.main.temp_min) + "°C";
-
-        ////MaxTemperature
-        var MaxTemperature = WidgetList.children[i].querySelector('.container.max-temperature');
-        MaxTemperature.children[1].innerText = Math.round(data.main.temp_max) + "°C";    
-        
-        ////Luftfreuchtigkeit
-        var Humidity = WidgetList.children[i].querySelector('.container.humidity');
-        Humidity.children[1].innerText = Math.round(data.main.humidity) + "%";
-        
-        ////Luftdruck
-        var Pressure = WidgetList.children[i].querySelector('.container.pressure');
-        Pressure.children[1].innerText = Math.round(data.main.pressure) + "hPa";
-        
-        ////Windgeschwindigkeit
-        var WindSpeed = WidgetList.children[i].querySelector('.container.wind');
-        WindSpeed.children[1].innerText = data.wind.speed;        
-    }
-    else //Stadt existiert noch nicht
-    {
-        //Template holen und duplizieren
-        var template = document.querySelector('#weather-widget-template');
-        var WeatherTemplate = template.cloneNode(true);
-        
-        //Data-city Befüllen
-        var datacity = WeatherTemplate.content.querySelector('.widget');
-        datacity.attributes[1].value = data.name;
-        
-        //Icon anpassen
-        //"http://openweathermap.org/img/wn/10d@2x.png"
-        var iconsource = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        var icon = WeatherTemplate.content.querySelector('.icon');
-        icon.children[0].src = iconsource;    
-        
-        //Ort + Temperatur Befüllen
-        // Template --> span.data --> span.main
-
-        var CityandTemp = WeatherTemplate.content.querySelector('.main');
-
-        var LocationName = document.createElement("span");
-        LocationName.textContent = data.name + ", " + Math.round(data.main.temp) + "°C";
-        CityandTemp.appendChild(LocationName);
-        
-        //min Temperature
-        var MinTemperature = WeatherTemplate.content.querySelector('.container.min-temperature');
-        MinTemperature.innerText = Math.round(data.main.temp_min) + "°C";
-
-        ////MaxTemperature
-        var MaxTemperature = WeatherTemplate.content.querySelector('.container.max-temperature');
-        MaxTemperature.children[1].innerText = Math.round(data.main.temp_max) + "°C";    
-        
-        ////Luftfreuchtigkeit
-        var Humidity = WeatherTemplate.content.querySelector('.container.humidity');
-        Humidity.children[1].innerText = Math.round(data.main.humidity) + "%";
-        
-        ////Luftdruck
-        var Pressure = WeatherTemplate.content.querySelector('.container.pressure');
-        Pressure.children[1].innerText = Math.round(data.main.pressure) + "hPa";
-        
-        ////Windgeschwindigkeit
-        var WindSpeed = WeatherTemplate.content.querySelector('.container.wind');
-        WindSpeed.children[1].innerText = data.wind.speed;
-        
-        var DeleteButton = WeatherTemplate.content.querySelector('.delete');
-        DeleteButton.addEventListener("click", function(){
-            litter(this.parentNode.parentNode.attributes[1].textContent);
-            deleteFromLocalStorage(this.parentNode.parentNode.attributes[1].textContent);
-        });
-
-        var UpdateButton = WeatherTemplate.content.querySelector('.update');
-        UpdateButton.addEventListener("click", function(){
-            getData(this.parentNode.parentNode.attributes[1].textContent);
-        });
-
-        WidgetList.appendChild(WeatherTemplate.content);
-    }
-
  }
 
  // speicher jede neu hinzugefügte Stadt im localstorage
@@ -191,7 +69,6 @@
          cityList = JSON.parse(localStorage.getItem("cities"));
             for (let index = 0; index < cityList.length; index++) {
                 let city = cityList[index];
-                console.log(city);
                 if(city!== ""){
                     getData(city);
                 }
@@ -202,7 +79,7 @@
  function litter(City){
     var rubbish = document.getElementsByTagName('ul')[0];
     
-    for(var i = 1; i <= rubbish.children.length; i++){
+    for(let i = 1; i <= rubbish.children.length; i++){
         if(City === rubbish.children[i].attributes[1].value){
             deleteFromLocalStorage(rubbish.children[i].attributes[1].value);
             rubbish.removeChild(rubbish.children[i]);
---
+++ resources/js/ui/WeatherWidget.js
@@ -0,0 +1,73 @@
+/* eslint-env browser */
+
+var widgetList = document.getElementsByTagName("ul")[0],
+    CityExists = 0;
+
+class WeatherWidget {
+
+    addWeatherWidget(data, litter, getData, deleteFromLocalStorage){
+        //prüft ob Stadt bereits in Liste existiert, falls nicht wird diese hinzugefügt, falls doch aktualisiert
+
+    // Wird nur ausgeführt wenn bereits eine Stadt in der Liste enthalten ist
+    if (widgetList.childElementCount > 1){
+        for(let i = 1; i < widgetList.children.length; i++)
+        {
+            if(data.name === widgetList.children[i].attributes[1].value){
+                CityExists = i;
+                break;
+            }
+        }        
+    }
+     //Stadt Existiert bereits
+    if(CityExists > 0){
+        console.log("Stadt existiert bereits");         
+    } else {
+        
+        //Template wird dupliziert
+        let template = document.querySelector("#weather-widget-template"),
+            WeatherTemplate = template.cloneNode(true),
+            datacity = WeatherTemplate.content.querySelector(".widget"),
+            iconsource = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png",
+            icon = WeatherTemplate.content.querySelector(".icon"),
+            CityandTemp = WeatherTemplate.content.querySelector(".main"),
+            LocationName = document.createElement("span"),
+            MinTemperature = WeatherTemplate.content.querySelector(".container.min-temperature"),
+            MaxTemperature = WeatherTemplate.content.querySelector(".container.max-temperature"),
+            Humidity = WeatherTemplate.content.querySelector(".container.humidity"),
+            Pressure = WeatherTemplate.content.querySelector(".container.pressure"),
+            WindSpeed = WeatherTemplate.content.querySelector(".container.wind"),
+            DeleteButton = WeatherTemplate.content.querySelector(".delete"),
+            UpdateButton = WeatherTemplate.content.querySelector(".update");
+        
+            //Data-city wird befüllt
+            datacity.attributes[1].value = data.name;
+            icon.children[0].src = iconsource; 
+            LocationName.textContent = data.name + ", " + Math.round(data.main.temp) + "°C";
+            CityandTemp.appendChild(LocationName);
+            
+            //min Temperature
+            MinTemperature.innerText = Math.round(data.main.temp_min) + "°C";
+            //MaxTemperature
+            MaxTemperature.children[1].innerText = Math.round(data.main.temp_max) + "°C";    
+            //Luftfreuchtigkeit
+            Humidity.children[1].innerText = Math.round(data.main.humidity) + "%";
+            //Luftdruck
+            Pressure.children[1].innerText = Math.round(data.main.pressure) + "hPa";
+            //Windgeschwindigkeit
+            WindSpeed.children[1].innerText = data.wind.speed;
+            
+            DeleteButton.addEventListener("click", function(){
+                litter(this.parentNode.parentNode.attributes[1].textContent);
+                deleteFromLocalStorage(this.parentNode.parentNode.attributes[1].textContent);
+            });
+
+            UpdateButton.addEventListener("click", function(){
+                getData(this.parentNode.parentNode.attributes[1].textContent);
+            });
+
+                widgetList.appendChild(WeatherTemplate.content);
+        }
+    }
+}
+
+export default WeatherWidget;
