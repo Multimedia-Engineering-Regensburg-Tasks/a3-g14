04ba282da07a6c5becf845c1603931b8a7b05c4c
Your Name <janik.schoebel@gmail.com>
2021-06-10 18:55:02 +0200

    minor commit

--- resources/js/ui/WeatherWidget.js
+++ resources/js/ui/WeatherWidget.js
@@ -20,7 +20,29 @@ class WeatherWidget {
     }
      //Stadt Existiert bereits
     if(CityExists > 0){
-        console.log("Stadt existiert bereits");         
+        let iconsource = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png",
+            icon = widgetList.children[i].querySelector(".icon"),
+            CityandTemp = widgetList.children[i].querySelector(".main"),
+            CurrentString = CityandTemp.querySelector("span"),
+            MinTemperature = widgetList.children[i].querySelector(".container.min-temperature"),
+            MaxTemperature = widgetList.children[i].querySelector(".container.max-temperature"),
+            Humidity = widgetList.children[i].querySelector(".container.humidity"),
+            Pressure = widgetList.children[i].querySelector(".container.pressure"),
+            WindSpeed = widgetList.children[i].querySelector(".container.wind"); 
+
+            CurrentString.textContent = data.name + ", " + Math.round(data.main.temp) + "°C";
+            icon.children[0].src = iconsource; 
+            //min Temperature
+            MinTemperature.innerText = Math.round(data.main.temp_min) + "°C";
+            //MaxTemperature
+            MaxTemperature.children[1].innerText = Math.round(data.main.temp_max) + "°C";
+            //Luftfreuchtigkeit    
+            Humidity.children[1].innerText = Math.round(data.main.humidity) + "%"; 
+            //Luftdruck
+            Pressure.children[1].innerText = Math.round(data.main.pressure) + "hPa";
+            //Windgeschwindigkeit
+            WindSpeed.children[1].innerText = data.wind.speed;
+            
     } else {
         
         //Template wird dupliziert
